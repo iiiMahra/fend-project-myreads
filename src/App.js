03892bcd6ff78d3d529ad17024658fa5e6843b22
Mahra-almanei <iiimahram@gmail.com>
2018-11-28 11:34:30 +0400

    chore: Importing the UI components and creating book state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import {Route} from 'react-router-dom'
 import './App.css'
+import SearchPage from './SearchPage'
+import MainPage from './MainPage'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books:[]
   }
 
   render() {

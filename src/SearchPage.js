3855b4d671e94230acfcd56f377bdd97e86cc9b5
Mahra-almanei <iiimahram@gmail.com>
2018-11-30 10:46:43 +0400

    feat: Creating SearchPage  UI

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,35 +1,82 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import Book from './Book'
+import {Link} from 'react-router-dom'
 
 
-function Book (props){
-  return(
 
-    <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ 
-              width: 128,
-              height: 193,
-              backgroundImage:`url("${props.book.imageLinks ?
-                 props.book.imageLinks.thumbnail :
-                 ''}")`}}></div>
-            <div className="book-shelf-changer">
-              <select onChange={(event) => props.shelfUpdate(props.book, event.target.value)}
-              value={props.book.shelf === undefined ? props.book.shelf='none' : props.book.shelf} 
-                       >
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-                
-              </select>
-            </div>
-          </div>
-          <div className="book-title">{props.book.title}</div>
-          <div className="book-authors">{props.book.authors || ''}</div>
-        </div>
-  )
+class SearchPage extends Component {
+    state = {
+    query: '',
+    searchedBooks: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({query: query })
+    this.showSearchedBooks(query)
+
+  }
+
+showSearchedBooks = (query) => {
+  if(query) {
+	BooksAPI.search(query).then((searchedBooks) => {
+// if there is a query and it's not correct the searchedBooks will be an empty array    
+      if(searchedBooks.error) {
+        this.setState({ searchedBooks: []})
+      }
+// if there is a query and it's correct fetch the array      
+      else{
+        this.setState({searchedBooks:searchedBooks})
+      }
+    })
+  }
+// if there is no query the searchedBooks will be an empty array  
+   else{ 
+    this.setState({ searchedBooks: []})
+  }
 }
 
- 
-export default Book
+  render() {
+/*if a book is assigned to a shelf on the main page and that book appears on the search page, 
+the correct shelf should be selected on the search page.*/    
+      this.state.searchedBooks.map((searchedBook)=>{
+      return(
+        this.props.books.map((book)=>{
+          searchedBook.id===book.id ? searchedBook.shelf=book.shelf:"";
+        })
+      )
+    })
+    return (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to="/" className="close-search">Close</Link>
+              <div className="search-books-input-wrapper">
+                <input type="text" placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(event) => this.updateQuery(event.target.value)}/>
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+              {
+              	this.state.searchedBooks.map(searchedBook => {
+              		return(
+              			<li key={searchedBook.id}>
+	              			<Book 
+	              			  book={searchedBook}
+	                			shelfUpdate={this.props.shelfUpdate}
+	              			/>
+              			</li>
+              		);
+              	})
+              }
+
+              </ol>
+            </div>
+          </div>
+        )
+  }
+}
+
+export default SearchPage
